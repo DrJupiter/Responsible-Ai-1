ed9c8daf31158aac7dbfbdad9b63fa4f268a62f8
Maplewarrior <michaelharborg@gmail.com>
2023-03-05 15:29:22 +0100

    cleaned up main

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import torch
 from dataload import CatalanDataset, datasplit, convert_dataload 
 import numpy as np
 import random
+import time 
 
 # Reducability
 seed = 42
@@ -10,46 +11,92 @@ torch.manual_seed(seed)
 random.seed(seed)
 np.random.seed(seed)
 
-# Constants
-DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-LEARNING_RATE = 1e-4
-EPOCHS = 700
+class cfg:
+   def __init__(self):
+      self.data_path = './data/preprocessed.csv'
+      self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+      self.lr = 1e-4
+      self.start_epoch = 0
+      self.n_epochs = 700
+      self.print_freq = 50
 
-# Initialize data
-dataset = CatalanDataset('./data/preprocessed.csv')
-train, validation, test = convert_dataload(datasplit(dataset))
-_x, _y = next(iter(train))
 
-# Initialize model
-model = Net_Logistic(_x.size()[1]) 
-model= torch.nn.DataParallel(model)
-model.to(DEVICE)
-model.train()
+def main():
+  CFG = cfg()  
 
-# Optimizer and Loss
-optimizer = torch.optim.Adam(model.parameters(),lr=LEARNING_RATE)
-loss_fn = torch.nn.BCELoss()
+  ### Load data, split and initialize dataloaders ###
+  dataset = CatalanDataset(CFG.data_path)
+  DataloaderTrain, DataloaderVal, DataloaderTest = convert_dataload(datasplit(dataset))
+  _x, _y = next(iter(DataloaderTrain))
 
-# Train
-losses = []
-accur = []
+  ### Initialize model, optimizer and loss ###
+  model = Net_Logistic(_x.size()[1]) 
+  model= torch.nn.DataParallel(model)
+  model.to(CFG.device)
+  
+  optimizer = torch.optim.Adam(model.parameters(), lr=CFG.lr)
 
-for i in range(EPOCHS):
-  for j,(feature,target) in enumerate(train):
-    feature, target = feature.to(DEVICE), target.to(DEVICE)
-    #calculate output
-    output = model(feature)
- 
-    #calculate loss
-    loss = loss_fn(output,target.reshape(-1,1))
- 
-    #accuracy
+  criterion = torch.nn.BCELoss().to(CFG.device)
+
+  train_losses, val_losses, train_accs, val_accs = [], [], [], []
+
+  ### Start training loop ###
+  for i in range(CFG.start_epoch, CFG.n_epochs):
+     
+     train_loss, train_acc = train(model, DataloaderTrain, optimizer, criterion, CFG)
+     train_losses.append(train_loss)
+     train_accs.append(train_acc)
+
+     val_loss, val_acc = validate(model, DataloaderVal, criterion, CFG)
+     val_losses.append(val_loss)
+     val_accs.append(val_acc)
+
+def train(model, dataloader, optimizer, criterion, CFG):
+  model.train()
+  accs = []
+  losses = []
+  start = time.time()
+
+  for i, (ipt, trg) in enumerate(dataloader):
+    ipt = ipt.to(CFG.device)
+    trg = trg.to(CFG.device)
+
+    out = model(ipt)
+    loss = criterion(out, trg)
+    losses.append(loss.detach().cpu())
+
     optimizer.zero_grad()
     loss.backward()
     optimizer.step()
 
+    end = time.time()
+    if i % CFG.print_freq == 0:
+       print(f"Time elapsed: {(end-start)/60:.4f} min\nAvg loss: {np.mean(losses)}")
+  return losses, accs
 
-model.eval()
+def validate(model, dataloader, criterion, CFG):
+  model.eval()
+  accs = []
+  losses = []
+  start = time.time()
+
+  with torch.no_grad():
+    for i, (ipt, trg) in enumerate(dataloader):
+      ipt = ipt.to(CFG.device)
+      trg = trg.to(CFG.device)
+
+      out = model(ipt)
+      loss = criterion(out, trg)
+      losses.append(loss.cpu())
+
+      end = time.time()
+      if i % CFG.print_freq == 0:
+        print(f"Time elapsed: {(end-start)/60:.4f} min\nAvg loss: {np.mean(losses)}")
+
+  return losses, accs
+
+
+
 val_loss = []
 for feature, target in validation:
     feature, target = feature.to(DEVICE), target.to(DEVICE)
